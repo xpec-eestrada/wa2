8bff49d4b1d1349a8d90a900973694a655350e77
Erick Estrada <eestrada@xpectrumtech.com>
2018-01-26 03:19:26 +0000

    block en home

--- code/Xpectrum/Globals/Block/Product/Widget/ProductsList.php
+++ code/Xpectrum/Globals/Block/Product/Widget/ProductsList.php
@@ -164,6 +164,7 @@ class ProductsList extends \Magento\CatalogWidget\Block\Product\ProductsList{
         
         //$collection->getSelect()->where("stock_status_index.stock_status = 1");
         $this->_stockFilter->addInStockFilterToCollection($collection);
+        error_log($collection->getSelect()->__toString());
         return $collection;
     }
 
