2e3c15a9ac728f9f53fe91ade73b70263de4a7ac
Erick Estrada <eestrada@xpectrumtech.com>
2018-03-18 16:49:36 +0000

    modulo de newsletter

--- code/Xpectrum/NewsletterPopup/Model/Subscriber.php
+++ code/Xpectrum/NewsletterPopup/Model/Subscriber.php
@@ -27,7 +27,8 @@ class Subscriber extends \Magento\Newsletter\Model\Subscriber
             return $this;
         }
 
-
+        $tmp = $this->_scopeConfig->getValue('xpecnewsletter/general/id_cupon', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $this->ruleId = (isset($tmp) && is_numeric($tmp) && $tmp>0)?$tmp:$this->ruleId;
         $this->inlineTranslation->suspend();
         $objectManager                  = \Magento\Framework\App\ObjectManager::getInstance();
         $this->couponManagementService  = $objectManager->get('Magento\SalesRule\Model\Service\CouponManagementService');
@@ -36,7 +37,6 @@ class Subscriber extends \Magento\Newsletter\Model\Subscriber
         $resource                       = $objectManager->get('Magento\Framework\App\ResourceConnection');
         $connection                     = $resource->getConnection();
         $tableName                      = $resource->getTableName('xpec_email_cupon_newsletter');
-
 
         $sql    = "INSERT INTO ".$tableName."(email,cupon,estado) VALUES('".$this->getEmail()."','".$cupon."','A')";
         $connection->query($sql);
