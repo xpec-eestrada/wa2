aa3c27a0cff9e8c3c321f166f6d34ad8a41556fa
xpec-eestrada <eestrada@xpectrumtech.com>
2018-04-23 19:55:26 -0500

    log

--- code/Xpectrum/Servipag/Controller/Payment/ResponseXml4.php
+++ code/Xpectrum/Servipag/Controller/Payment/ResponseXml4.php
@@ -45,28 +45,37 @@ class ResponseXml4 extends \Magento\Framework\App\Action\Action
         
         $incrementId = $this->checkoutSession->getLastRealOrderId();
         $order = $this->salesOrder->loadByIncrementId($incrementId);
+
+        error_log("servipag init: ".$incrementId);
         
         if ($incrementId) {
+            error_log("servipag: increment");
             if ($xml4) {
+                error_log("servipag: xml4");
                 $result = $this->servipagPayment->validateXml4($xml4);
+                error_log("servipag codigo: ".$result['codigo']);
                 if (isset($result['codigo']) && $result['codigo'] === 0) {
+                    error_log("servipag processing");
                     $order->setState('processing')->setStatus('processing'); 
                     $order->save();
 
                     $this->_redirect('checkout/onepage/success/', array('_secure' => false));
-                } else {                
+                } else {
+                    error_log("servipag cancel");
                     $order->cancel()->setState(\Magento\Sales\Model\Order::STATE_CANCELED, true, 'Canceled')->save();
 
                     $this->messageManager->addError('Le informamos que su orden ' . $incrementId . ', realizada el ' . $order->getCreatedAt() . ' termin&oacute; de forma inesperada.');
                     $this->_redirect('checkout/onepage/failure/', array('_secure' => false));
                 }
             } else {
+                error_log("servipag processing");
                 $order->cancel()->setState(\Magento\Sales\Model\Order::STATE_CANCELED, true, 'Canceled')->save();
 
                 $this->messageManager->addError('Le informamos que su orden ' . $incrementId . ', realizada el ' . $order->getCreatedAt() . ' termin&oacute; de forma inesperada.');
                 $this->_redirect('checkout/onepage/failure/', array('_secure' => false));
             }
         } else {
+            error_log("servipag no increment");
             $this->messageManager->addError('Hubo un error en el pago. N&uacute;mero de pedido inv&aacute;lido.');
             $this->_redirect('checkout/onepage/failure/', array('_secure' => false));
         }
